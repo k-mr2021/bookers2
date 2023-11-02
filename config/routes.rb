43748d595572054f360c9cdde6a72ddbc5e4468c
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-11-02 15:51:50 +0000

    [Finish] 課題3__2日目

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  devise_for :users
+  resources :books, only: [:new, :index, :show]
+  resources :users, only: [:show, :edit]
   get 'homes/about' => 'homes#about', as: 'about'
 end
