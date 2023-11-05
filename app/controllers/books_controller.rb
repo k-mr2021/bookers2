c967eeacb783e87e460d53490bcd0b6b07369892
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-11-05 15:31:17 +0000

    [Finish] 課題3__5日目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,9 @@ class BooksController < ApplicationController
   end
   # 詳細
   def show
-    
+    @book_found = Book.find(params[:id])
+    @user = @book_found.user
+    @new_book = Book.new
   end
   # 保存
   def create
@@ -14,6 +16,22 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
+  end
+  # 編集
+  def edit
+    @book_found = Book.find(params[:id])
+  end
+  # 更新
+  def update
+    @book_found = Book.find(params[:id])
+    @book_found.update(book_params)
+    redirect_to book_path(@book_found.id)
+  end
+  # 削除
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
@@ -23,3 +41,6 @@ class BooksController < ApplicationController
   end
   
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:new, :index, :show, :create]
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :index]
   get 'homes/about' => 'homes#about', as: 'about'
 end
+
+
