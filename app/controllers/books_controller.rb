6448c10d589c5c72b2640687b06fa2d5d81e3cee
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-11-05 04:59:47 +0000

    BooksIndex作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,25 @@
 class BooksController < ApplicationController
-  
+  # 一覧、新規投稿
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+  # 詳細
   def show
     
+  end
+  # 保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   
   has_one_attached :profile_image
   
-  def get_profile_image(width, heigh)
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
@@ -16,3 +16,4 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
+
